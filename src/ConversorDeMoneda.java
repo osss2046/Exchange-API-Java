a1a6df57a21f1c2e1bfbfb2cfcc6204ea5e36f80
Oscar Acuña <oscarboris@outlook.cl>
2024-07-13 16:08:47 -0400

    actualización funcionando

--- src/ConversorDeMoneda.java
+++ src/ConversorDeMoneda.java
@@ -1,2 +1,21 @@
+import com.google.gson.Gson;
+import java.util.Map;
+
 public class ConversorDeMoneda {
+
+    public static ApiResponse getExchangeRates(String baseCurrency) throws Exception {
+        String jsonResponse = HttpCliente.get(baseCurrency);
+        Gson gson = new Gson();
+        return gson.fromJson(jsonResponse, ApiResponse.class);
+    }
+
+    public static double convert(String fromCurrency, String toCurrency, double amount) throws Exception {
+        ApiResponse apiResponse = getExchangeRates(fromCurrency);
+        Map<String, Double> rates = apiResponse.getConversion_rates();
+        if (rates.containsKey(toCurrency)) {
+            return amount * rates.get(toCurrency);
+        } else {
+            throw new IllegalArgumentException("Moneda no Encontrada: " + toCurrency);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Bienvenido al conversor de monedas");
+            System.out.println("1. USD a CLP");
+            System.out.println("2. BRL a USD");
+            System.out.println("3. EUR a CLP");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            int option = scanner.nextInt();
+
+            if (option == 4) {
+                break;
+            }
+
+            System.out.print("Ingrese el valor a convertir: ");
+            double amount = scanner.nextDouble();
+
+            try {
+                double result = 0;
+                switch (option) {
+                    case 1:
+                        result = ConversorDeMoneda.convert("USD", "CLP", amount);
+                        break;
+                    case 2:
+                        result = ConversorDeMoneda.convert("BRL", "USD", amount);
+                        break;
+                    case 3:
+                        result = ConversorDeMoneda.convert("EUR", "CLP", amount);
+                        break;
+                    default:
+                        System.out.println("Opción no válida.");
+                        continue;
+                }
+                System.out.println("El resultado es: " + result);
+            } catch (Exception e) {
+                System.out.println("Error al convertir: " + e.getMessage());
+            }
+        }
+        scanner.close();
     }
 }
